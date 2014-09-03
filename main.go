1e6e6415f02631d0502831846514da5d3b861704
Quinn <quintessenceanx@gmail.com>
2014-09-03 16:38:15 -0400

    added missing flag.Parse()

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 //  "github.com/russross/blackfriday"
   "os/exec"
   "flag"
+  "fmt"
 )
 
 func main(){
@@ -13,12 +14,15 @@ func main(){
   flag.StringVar(&sourceFile, "inputFile", "", "File to be converted to HTML presentation")
   flag.StringVar(&destinationDir, "outputDir", "", "Directory where HTML presentation will be written")
  
+  flag.Parse()
+
   copyCommand := exec.Command("cp", "-rf", sourceFile, destinationDir)
   err := copyCommand.Run()
   
   if err != nil {
    output, _ := copyCommand.Output()
    println(output)
-   println(sourceFile, destinationDir)
+   fmt.Println("sourcefile: ", sourceFile)
+   fmt.Println("destDir: ", destinationDir)
   }
 }
