13e0ecd8d5892034a09bacfcf6fe591b69efc32d
Quinn <quintessenceanx@gmail.com>
2014-09-03 16:33:02 -0400

    Trying to use flags

--- main.go
+++ main.go
@@ -1,17 +1,24 @@
 package main
 
-//import (
+import (
 //  "github.com/russross/blackfriday"
-//)
-
-import "os/exec"
+  "os/exec"
+  "flag"
+)
 
 func main(){
-  sourceFile := "../temp/myfile.md"
-  destinationDir := "../temp/myfile"
+  var sourceFile string
+  var destinationDir string
+
+  flag.StringVar(&sourceFile, "inputFile", "", "File to be converted to HTML presentation")
+  flag.StringVar(&destinationDir, "outputDir", "", "Directory where HTML presentation will be written")
+ 
   copyCommand := exec.Command("cp", "-rf", sourceFile, destinationDir)
   err := copyCommand.Run()
+  
   if err != nil {
-    return
+   output, _ := copyCommand.Output()
+   println(output)
+   println(sourceFile, destinationDir)
   }
 }
