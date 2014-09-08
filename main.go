21d667edc0ffe2a11c621d884a443d4721d68ce7
Quinn <quintessenceanx@gmail.com>
2014-09-08 09:12:13 -0400

    refactoring

--- main.go
+++ main.go
@@ -9,29 +9,48 @@ import (
   "strings"
 )
 
+func updateDestinationDirPath(currentDestDir string, currentInputFile string, newDirFlag bool) string {
+  if currentDestDir == "" || newDirFlag {
+    inputPathArray := strings.Split(currentInputFile, "/")
+    return currentDestDir + strings.Split(inputPathArray[len(inputPathArray)-1], ".")[0]
+  }
+
+  return currentDestDir
+}
+
+func fileOrDirectoryDoesNotExist(inputPath string) bool {
+  if _, err := os.Stat(inputPath); os.IsNotExist(err) {
+    return true
+  }
+  return false
+}
+
 func main(){
   var sourceFile string
   var destinationDir string
   var verboseMode bool
+  var newDir bool
 
   flag.StringVar(&sourceFile, "inputFile", "", "File to be converted to HTML presentation")
   flag.StringVar(&destinationDir, "outputDir", "", "Directory where HTML presentation will be written")
   flag.BoolVar(&verboseMode, "v", false, "Verbose mode prints progress messages to screen")
+  flag.BoolVar(&newDir, "newDir", false, "Creates a new directory named after the file")
 
   flag.Parse()
 
-  if destinationDir == "" {
-    inputPathArray := strings.Split(sourceFile, "/")
-    destinationDir = strings.Split(inputPathArray[len(inputPathArray)-1], ".")[0]
-  }
-
-  if _, err := os.Stat(sourceFile); os.IsNotExist(err) {
-    fmt.Printf("No such file or directory: %s\nExiting program.\n", sourceFile)
+  if fileOrDirectoryDoesNotExist(sourceFile){ 
+    if verboseMode {
+      fmt.Printf("No such file or directory: %s\nExiting program.\n", sourceFile)
+    }
     return
   }
 
-  if _, err := os.Stat(destinationDir); os.IsNotExist(err) {
-    fmt.Printf("No such directory, creating new directory: %s\n", destinationDir)
+  destinationDir = updateDestinationDirPath(destinationDir, sourceFile, newDir)
+
+  if fileOrDirectoryDoesNotExist(destinationDir){ 
+    if verboseMode {
+      fmt.Printf("No such directory, creating new directory: %s\n", destinationDir)
+    }
     mkdirCommand := exec.Command("mkdir", destinationDir)
     mkdirErr := mkdirCommand.Run()
     if mkdirErr != nil {
@@ -53,8 +72,5 @@ func main(){
   }
 
   fmt.Printf("Successfully copied files.\nExiting.\n")
-
-  fmt.Println("Value of v: ", verboseMode)
-  fmt.Printf("%q\n", strings.Split(sourceFile, "/"))
 
 }
